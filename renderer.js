2cea1aa1b1fc35eadc6d55ee0f2780398db04bf7
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-05-13 09:58:37 +0300

    Add visitor tracking to all pages

--- renderer.js
+++ renderer.js
@@ -17,39 +17,82 @@ MongoClient.connect(
 
 module.exports = {
     main: function(req, res){
+        let visitor = {
+            ip: req.header("x-forwarded-for") || req.connection.remoteAddress,
+            dateTime: new Date(),
+            page: "main"
+        }
+        Visitor.insertOne(visitor).catch(()=>{});
         return res.sendFile(`${__dirname}/views/home.html`);
     },
 
     rentYourKitchen: function(req, res){
         let visitor = {
             ip: req.header("x-forwarded-for") || req.connection.remoteAddress,
-            dateTime: new Date()
+            dateTime: new Date(),
+            page: "rentyourkitchen"
         }
         Visitor.insertOne(visitor).catch(()=>{});
         return res.sendFile(`${__dirname}/views/rentYourKitchen.html`);
     },
 
     kitchenRental: function(req, res){
+        let visitor = {
+            ip: req.header("x-forwarded-for") || req.connection.remoteAddress,
+            dateTime: new Date(),
+            page: "kitchenrental"
+        }
+        Visitor.insertOne(visitor).catch(()=>{});
         return res.sendFile(`${__dirname}/views/kitchenRental.html`);
     },
 
     kitchenCo: function(req, res){
+        let visitor = {
+            ip: req.header("x-forwarded-for") || req.connection.remoteAddress,
+            dateTime: new Date(),
+            page: "kitchenco"
+        }
+        Visitor.insertOne(visitor).catch(()=>{});
         return res.sendFile(`${__dirname}/views/kitchenCo.html`);
     },
 
     kitchenCooks: function(req, res){
+        let visitor = {
+            ip: req.header("x-forwarded-for") || req.connection.remoteAddress,
+            dateTime: new Date(),
+            page: "kitchencooks"
+        }
+        Visitor.insertOne(visitor).catch(()=>{});
         return res.sendFile(`${__dirname}/views/kitchenCooks.html`);
     },
 
     inventoryManagement: function(req, res){
+        let visitor = {
+            ip: req.header("x-forwarded-for") || req.connection.remoteAddress,
+            dateTime: new Date(),
+            page: "inventorymanagement"
+        }
+        Visitor.insertOne(visitor).catch(()=>{});
         return res.sendFile(`${__dirname}/views/inventoryManagement.html`);
     },
 
     kytchnDelivery: function(req, res){
+        let visitor = {
+            ip: req.header("x-forwarded-for") || req.connection.remoteAddress,
+            dateTime: new Date(),
+            page: "kytchndelivery"
+        }
+        Visitor.insertOne(visitor).catch(()=>{});
         return res.sendFile(`${__dirname}/views/kytchnDelivery.html`);
     },
 
     kytchnOrder: function(req, res){
+        let visitor = {
+            ip: req.header("x-forwarded-for") || req.connection.remoteAddress,
+            dateTime: new Date(),
+            page: "kytchnorder"
+        }
+        Visitor.insertOne(visitor).catch(()=>{});
         return res.sendFile(`${__dirname}/views/kytchnOrder.html`);
     },
 
